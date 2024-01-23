5ebd28311c0be93b74fe76715c3d98b6df0c7896
Тимур Калимуллин <kalimullin.tt@phystech.edu>
2024-01-23 17:37:33 +0300

    Initial Commit

---
+++ pomosh--2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomosh--2024
+//
+//  Created by Тимур Калимуллин on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
